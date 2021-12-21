f7a6b728da3d304b31fede0b08af6d52662339dc
mohamed gamal elnagar <96170583+devmohamedgamal919@users.noreply.github.com>
2021-12-21 16:54:47 +0200

    first commit

---
+++ storage/framework/views/9ba92c33cd95fa2e2c1e3a0c7767414c4a7a33c3.php
@@ -0,0 +1,2 @@
+<h1>Your Username : <?php echo e($name); ?></h1>
+<h1>Your Password : <?php echo e($password); ?></h1><?php /**PATH /home/u623654606/domains/betnaapp.com/public_html/cleversale/resources/views/mail/user_details.blade.php ENDPATH**/ ?>
